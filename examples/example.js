fa5181554e75e6930f15ab4dc6f85d99249db221
richard koehl <koehl.richard@gmail.com>
2017-04-15 20:47:43 +0200

    construct lib

--- examples/example.js
+++ examples/example.js
@@ -1,5 +1,37 @@
-var generator = require("../index");
+let Generator = require("../index");
 
-generator.printMsg();
+var generator = new Generator();
 
-generator.build(["b", "a"]);
+//Build option
+generator.build(
+    [
+        {
+            code: "entity",
+            type: "string",
+            option: ["-e", "--entity"],
+            description: "Entities to construct java / js / ftl / less files",
+            required: true
+        },
+        {
+            code: "from",
+            type: "folder",
+            option: ["-f", "--from"],
+            description: "Template folder",
+            required: true,
+            validate: true
+        },
+        {
+            code: "output",
+            type: "file",
+            option: ["-o", "--output"],
+            description: "output folder",
+            required: true
+        }
+    ]
+);
+
+//Valid model
+generator.validate();
+
+//Get command
+generator.launchAction("entity", generator.replaceInFile);
--- index.js
+++ index.js
@@ -1,39 +1,127 @@
-var exports = module.exports = {};
+#!/usr/bin/env node
+'use strict';
 
-const strictNbArgs = 3;
+const util = require('util');
+const fs = require('fs');
+const asciiArt = require('ascii-art');
+const Mustache = require('mustache');
 
-exports.printMsg = function () {
-    console.log("This is a message from the demo package");
-};
+let ArgumentParser = require('./node_modules/argparse').ArgumentParser;
 
-exports.build = function (args, data) {
+var generator = function () {
 
-    clean(args);
+    let parser = new ArgumentParser({
+        version: '0.0.1',
+        addHelp: true,
+        description: 'Generator-catalog'
+    });
+    let data = {};
 
-    if (args === undefined || args.length !== strictNbArgs) {
-        logInvalidArgs(args);
-        return;
+    var self = this;
+
+    self.printHelloMsg = function (callback) {
+        asciiArt.font('Generator', 'Doom', 'red', function (rendered) {
+            console.log("--------------------------------------------------------");
+            console.log(rendered);
+            console.log("--------------------------------------------------------");
+            callback();
+        });
+    };
+
+    self.build = function (args) {
+
+        args.forEach(function (a) {
+            parser.addArgument(
+                a.option,
+                {
+                    help: a.description,
+                    required: a.required
+                }
+            );
+        });
+
+        const parsedValues = parser.parseArgs();
+
+        data = args.map((a) => {
+            a.data = {};
+            a.data.value = parsedValues[a.code];
+            return a;
+        });
+
+    };
+
+    self.validate = function () {
+
+        data
+            .filter((d) => {
+                return d.validate;
+            })
+            .map((d) => {
+                if ((d.type === "folder" || d.type === "file") && !fs.existsSync(d.data.value.toString())) {
+                    throw new Error(`Invalid ${d.type} for ${d.data.value}`);
+                }
+
+            });
+
+    };
+
+    self.get = function (name, args) {
+        return args[name];
+    };
+
+    self.launchAction = function (name, callback) {
+        data.filter((d) => {
+            return d.code === name;
+        }).map((d) => {
+            callback(
+                data.filter((f) => {
+                    return f.code === "from"
+                }).map((f) => {
+                    return f.data.value;
+                }).toString(),
+                d.data,
+                data.filter((f) => {
+                    return f.code === "output"
+                }).map((f) => {
+                    return f.data.value;
+                }).toString()
+            )
+            ;
+        })
+
+    };
+
+    self.replaceInFile = function (path, view, output) {
+        log(`path : ${path}`);
+        log(`view: ${util.inspect(view)}`);
+        log(`output: ${output}`);
+        var iter = [];
+        if (!fs.lstatSync(path).isDirectory()) {
+            iter.push(path);
+        } else {
+            iter = fs.readdirSync(path).map((d)=>{
+                return `${path}/${d}`;
+            });
+        }
+        iter.forEach(function (path) {
+            fs.readFile(path, function (err, data) {
+                if (err) throw err;
+                let filepath = `${output}${new Date().getTime()}`;
+                fs.writeFile(filepath, Mustache.render(data.toString(), view), (err) => {
+                    if (err) throw err;
+                    log(`File saved ${filepath}`)
+                });
+            })
+        });
+    };
+
+    self.test = function () {
+        log("test");
     }
-
-    log("Configuration accepted");
-
-
 };
 
-var clean = function(args){
-    args.filter(function(a){
-        return a !== "node" && a !== "*.js"
-    })
+function log(msg) {
+    console.log(`${new Date()} [generator-catalog] ${util.inspect(msg)}`);
 }
 
-logUndefined = function (entity) {
-    console.log(entity, " is undefined")
-};
-
-logInvalidArgs = function (args) {
-    console.error("[IllegalArgsException] Expected", strictNbArgs, "instead of", args.length, "\nargs", args);
-};
-
-log = function (entity) {
-    console.log(entity)
-};
+module.exports = generator;
